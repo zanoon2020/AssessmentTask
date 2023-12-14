78767f2624eaa7a7720e2d9158f4307f692dc4fc
Ahmed Gamal Zanoon <65794900+zanoon2020@users.noreply.github.com>
2023-12-14 10:58:52 +0200

    update Run result

--- testCrewAssessmentTask/generate_allure_report.bat
+++ testCrewAssessmentTask/generate_allure_report.bat
@@ -1,6 +1,6 @@
 @echo off
 :: If you already have a valid JAVA_HOME environment variable set, feel free to comment the below two lines
-set JAVA_HOME=C:\Program Files\Java\jdk-17.0.1
+set JAVA_HOME=D:\openjdk-21_windows-x64_bin\jdk-21
 set path=%JAVA_HOME%\bin;%path%
 set path=C:\Users\Ahmed Zanoon\.m2\repository\allure\allure-2.25.0\bin;%path%
 allure serve allure-results -h localhost
